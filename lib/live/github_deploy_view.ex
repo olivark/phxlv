21685deff08bb88d335505b9d9d32b8e1a1e4321
olivark <olivar.kevin@gmail.com>
2019-05-04 20:06:31 +0200

    live view working example

--- lib/live/github_deploy_view.ex
+++ lib/live/github_deploy_view.ex
@@ -1,4 +1,4 @@
-defmodule PhxliveviewWeb.GithubDeployView2 do
+defmodule PhxliveviewWeb.GithubDeployView do
   use Phoenix.LiveView
 
   def render(assigns) do
@@ -19,30 +19,7 @@ defmodule PhxliveviewWeb.GithubDeployView2 do
   end
 
   def handle_event("github_deploy", _value, socket) do
-    :ok = PhxliveviewWeb.start_deploy()
-    send(self(), :create_org)
+    # do the deploy process
     {:noreply, assign(socket, deploy_step: "Starting deploy...")}
-  end
-
-  def handle_info(:create_org, socket) do
-    {:ok, org} = PhxliveviewWeb.create_org()
-    send(self(), {:create_repo, org})
-    {:noreply, assign(socket, deploy_step: "Creating GitHub org...")}
-  end
-
-  def handle_info({:create_repo, org}, socket) do
-    {:ok, repo} = PhxliveviewWeb.create_repo(org)
-    send(self(), {:push_contents, repo})
-    {:noreply, assign(socket, deploy_step: "Creating GitHub repo...")}
-  end
-
-  def handle_info({:push_contents, repo}, socket) do
-    :ok = PhxliveviewWeb.push_contents(repo)
-    send(self(), :done)
-    {:noreply, assign(socket, deploy_step: "Pushing to repo...")}
-  end
-
-  def handle_info(:done, socket) do
-    {:noreply, assign(socket, deploy_step: "Done!")}
   end
 end
--- lib/phxliveview_web/views/page_view.ex
+++ lib/phxliveview_web/views/page_view.ex
@@ -1,11 +1,3 @@
-# defmodule PhxliveviewWeb.PageView do
-#  use PhxliveviewWeb, :view
-# end
-
-defmodule PhxliveviewWeb.GithubDeployView do
-  use Phoenix.LiveView
-
-  def render(assigns) do
-    PhxliveviewWeb.PageView.render("github_deploy.html", assigns)
-  end
+defmodule PhxliveviewWeb.PageView do
+  use PhxliveviewWeb, :view
 end
